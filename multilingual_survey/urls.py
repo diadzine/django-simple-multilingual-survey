680ea71b4ed6d9162010b46337222363ab081cda
Tim Plaisted <tim.plaisted@be-smart.ch>
2016-09-02 14:02:13 +0200

    Django 1.10 compatibility

--- multilingual_survey/urls.py
+++ multilingual_survey/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from multilingual_survey import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/$', views.survey_form, name='form'),
     url(r'^success/(?P<uuid>\w+)/$', views.survey_success, name='success'),
-)
+]
